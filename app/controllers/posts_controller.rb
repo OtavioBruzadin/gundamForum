4cdb832b1ce4d94d51331e3e9bb9a77cd4524005
Otávio Bruzadin <31077442+OtavioBruzadin@users.noreply.github.com>
2025-02-27 20:43:55 -0300

    Add: botao dislike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 
   # Página para criar um novo post
   def new
+
     @post = Post.new
   end
 
@@ -30,15 +31,31 @@ class PostsController < ApplicationController
   # Ação para curtir o post
   def like
     @post = Post.find(params[:id])
+    like = @post.likes.find_by(user: current_user)
+
+    if like
+      like.destroy
+    else
+      @post.likes.create(user: current_user)
+    end
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to root_path }
+    end
+  end
+
+  def deslike
+    @post = Post.find(params[:id])
 
     unless @post.likes.exists?(user: current_user)
-      @post.likes.create(user: current_user)
+      @post.likes.delete(user: current_user)
       flash[:notice] = "Você curtiu o post!"
     else
       flash[:alert] = "Você já curtiu este post!"
     end
 
-    redirect_to posts_path
+    redirect_to root_path
   end
 
   private
